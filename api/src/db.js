b9ce828c49e2528b32e6e1f62354c8ae11c01226
PrototypeK02 <100804258+PrototypeK02@users.noreply.github.com>
2022-07-15 00:12:59 -0300

    Deployment

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize,Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DATABASE_URL,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://postgres:totona88@localhost:5432/videogames`, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
